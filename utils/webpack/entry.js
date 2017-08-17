034535b388fe7ecd8db51e23d0206a6ab4fa03e5
Dennis <dennis.aldana@interactuamovil.com>
2017-08-17 17:00:13 -0600

    Initial commit, lesson 5

---
+++ utils/webpack/entry.js
@@ -0,0 +1,31 @@
+document.body.innerHTML += 'Hello from entry.js <br />'
+
+// mkdir js
+
+var lib = require('./js/lib.js')
+
+document.body.innerHTML += lib.hello
+
+// mkdir css
+// nano styles.css
+// var styles = require('./css/styles.css')
+
+// npm install css-loader
+
+// var styles = require('css-loader!./css/styles.css')
+
+// console.log(styles.toString())
+
+// document.body.innerHTML += '<style>' + styles + '</style>'
+
+// require('style-loader!css-loader!./css/styles.css')
+
+// --module-bind 'css=style!css'
+
+require('./css/styles.css')
+
+// webpack entry.js bundle.js --module-bind 'css=style-loader!css-loader'
+
+// npm install webpack-dev-server -g
+
+// http://127.0.0.1:8080/webpack-dev-server/bundle
---
+++ utils/webpack/webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    entry: "./entry.js",
+    output: {
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+		test: /\.css$/,
+                use: [
+                  {
+                     loader: 'style-loader'
+                  },
+                  {
+                     loader: 'css-loader'
+                  }
+                ]
+            }
+        ]
+    }
+};
